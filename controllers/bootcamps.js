0d1e34d0ce291b430b1b6aa3bbc0c2d4a5d25409
ritamwaikinda <srmwaikinda@gmail.com>
2023-10-16 20:13:33 -0400

    Mongoose first models

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,42 +1,110 @@
+const Bootcamp = require("../models/Bootcamp.js");
+
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res, next) => {
-	res
-		.status(200)
-		.json({ success: true, msg: "Show all bootcamps", hello: req.hello });
+exports.getBootcamps = async (req, res, next) => {
+	try {
+		const bootcamps = await Bootcamp.find();
+
+		res
+			.status(200)
+			.json({ success: true, count: bootcamps.length, data: bootcamps });
+	} catch (error) {
+		res.status(400).json({ success: false });
+	}
+
+	// res
+	// 	.status(200)
+	// 	.json({ success: true, msg: "Show all bootcamps", hello: req.hello });
 };
 
 // @desc    Get single bootcamps
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = (req, res, next) => {
-	res
-		.status(201)
-		.json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+	try {
+		const bootcamp = await Bootcamp.findById(req.params.id);
+		// if the id is correctly formatted but wrong
+		if (!bootcamp) {
+			return res.status(400).json({ success: false });
+		}
+		res.status(200).json({ success: true, data: bootcamp });
+	} catch (error) {
+		// if the id is badly formatted
+		res.status(400).json({ success: false });
+	}
+
+	// res
+	// 	.status(201)
+	// 	.json({ success: true, msg: `Show bootcamp ${req.params.id}` });
 };
 
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps/
 // @access  Private
-exports.createBootcamp = (req, res, next) => {
-	res.status(201).json({ success: true, msg: "Create new bootcamp" });
+exports.createBootcamp = async (req, res, next) => {
+	try {
+		const bootcamp = await Bootcamp.create(req.body);
+
+		res.status(201).json({
+			success: true,
+			data: bootcamp,
+		});
+	} catch (error) {
+		res.status(400).json({ success: false });
+	}
+	// console.log(req.body);
+	// res.status(201).json({ success: true, msg: "Create new bootcamp" });
 };
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootcamp = (req, res, next) => {
-	res
-		.status(201)
-		.json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+exports.updateBootcamp = async (req, res, next) => {
+	try {
+		// Takes 2+ arguments
+		const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			// Mongoose validator
+			runValidators: true,
+		});
+
+		if (!bootcamp) {
+			return res.status(400).json({ success: false });
+		}
+		//when testing with postman, if you have more than one bool field, it wants them wrapped in strings
+		res.status(200).json({
+			success: true,
+			data: bootcamp,
+		});
+	} catch (error) {
+		res.status(400).json({ success: false });
+	}
+	// res
+	// 	.status(201)
+	// 	.json({ success: true, msg: `Update bootcamp ${req.params.id}` });
 };
 
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Public
-exports.deleteBootcamp = (req, res, next) => {
-	res
-		.status(201)
-		.json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+exports.deleteBootcamp = async (req, res, next) => {
+	try {
+		const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+		if (!bootcamp) {
+			return res.status(400).json({ success: false });
+		}
+		res.status(200).json({
+			success: true,
+			data: {},
+		});
+	} catch (error) {
+		res.status(400).json({ success: false });
+	}
+
+	// res
+	// 	.status(201)
+	// 	.json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
 };
--- server.js
+++ server.js
@@ -2,13 +2,22 @@ const express = require("express");
 const dotenv = require("dotenv");
 // const logger = require("./middleware/logger");
 const morgan = require("morgan");
-//Route files
-const bootcamps = require("./routes/bootcamps");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 //won't just be ran by itself, must be used by app :)
 // app.use(logger);
@@ -20,6 +29,23 @@ if (process.env.NODE_ENV === "development") {
 //Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+	PORT,
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+	)
+);
+
+// Handle unhandled promise rejections
+
+process.on("unhandledRejection", (err, promise) => {
+	console.log(`Error: ${err.message}`.red);
+	// close server and exit process
+	server.close(() => process.exit(1));
+});
+
 /*
 GET/POST/PUT/DELETE
 api/v1/bootcamps
@@ -28,10 +54,3 @@ api/v1/reviews
 api/v1/auth
 api/v1/users
 */
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(
-	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
