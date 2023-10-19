eebce72eb24772fbef1982c16ff08c0a24896d87
ritamwaikinda <srmwaikinda@gmail.com>
2023-10-18 22:18:37 -0400

    seeder added

---
+++ seeder.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+// Load env variables
+dotenv.config({ path: "./config/config.env" });
+
+// Load models
+const Bootcamp = require("./models/Bootcamp");
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+// Read JSOn files
+const bootcamps = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+
+// Import into DB
+const importData = async () => {
+	try {
+		await Bootcamp.create(bootcamps);
+		console.log("Data imported...".green.inverse);
+		process.exit();
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+// Delete data
+const deleteData = async () => {
+	try {
+		// if you don't pass a parameter into deleteMany, it will just delete all of them
+		await Bootcamp.deleteMany();
+		console.log("Data destroyed...".red.inverse);
+		process.exit();
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+if (process.argv[2] === "import") {
+	importData();
+} else if (process.argv[2] === "delete") {
+	deleteData();
+}
