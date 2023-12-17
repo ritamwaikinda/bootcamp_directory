22a7f859c46a14cd9d4a3f63504567e24e2eca8f
ritamwaikinda <srmwaikinda@gmail.com>
2023-12-16 21:48:15 -0500

    review schema created

---
+++ models/Review.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		trim: true,
+		required: [true, "please add a title for the review"],
+		maxlength: 100,
+	},
+	text: {
+		type: String,
+		required: [true, "please add some text"],
+	},
+	rating: {
+		type: Number,
+		min: 1,
+		max: 10,
+		required: [true, "please add rating between 1 and 10"],
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	bootcamp: {
+		type: mongoose.Schema.ObjectId,
+		ref: "Bootcamp",
+		required: true,
+	},
+	user: {
+		type: mongoose.Schema.ObjectId,
+		ref: "User",
+		required: true,
+	},
+});
+
+module.exports = mongoose.model("Review", ReviewSchema);
