00be1b64f943e4ec2e299b3aa0ec6c3f6ab9a919
ritamwaikinda <srmwaikinda@gmail.com>
2023-12-18 04:27:09 -0500

    update a review

--- models/Review.js
+++ models/Review.js
@@ -33,4 +33,43 @@ const ReviewSchema = new mongoose.Schema({
 	},
 });
 
+// Prevent user from submitting more than one review per bootcamp
+ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
+
+// Static method to get average rating and save
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+	// creating an aggregated object. aggregate method returns a promise
+	const obj = await this.aggregate(
+		/*pipeline */ [
+			{
+				$match: { bootcamp: bootcampId },
+			},
+			{
+				$group: {
+					_id: "$bootcamp",
+					averageRating: { $avg: "$rating" },
+				},
+			},
+		]
+	);
+
+	try {
+		await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+			averageRating: obj[0].averageRating,
+		});
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+// Call getAverageRating after save
+ReviewSchema.post("save", function () {
+	this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Call getAverageRating before remove
+ReviewSchema.pre("deleteOne", function () {
+	this.constructor.getAverageRating(this.bootcamp);
+});
+
 module.exports = mongoose.model("Review", ReviewSchema);
